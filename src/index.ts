f8f817029c1420970dbcfe4ef69a1c52b4cc855f
Leko <leko@leko.moe>
2024-10-22 23:13:30 +0800

    fix: http server not launched

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import {bot} from './utils/telegram.js';
 const port = parseInt(process.env.PORT!);
 const host = process.env.HOST ?? '::';
 
-await bot.launch();
 process.on('SIGINT', () => bot.stop('SIGINT'));
 process.on('SIGTERM', () => bot.stop('SIGTERM'));
 
-app.listen({port, host}, () => {
-  console.log(`Server listening on http://${host}:${port}`);
-});
+await Promise.all([
+  app
+    .listen({port, host})
+    .then(() => console.log(`Server listening on http://${host}:${port}`)),
+  bot.launch(),
+]);
